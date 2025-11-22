1f7656c7792b2d0a2f84a7d4adfa614785b67858
yeo-li <parkseongyeol2110@gmail.com>
2025-11-22 12:34:08 +0900

    feat(MemoState): MemoState 구현

---
+++ src/main/kotlin/com/github/yeoli/devlog/domain/memo/repository/MemoState.kt
@@ -0,0 +1,33 @@
+package com.github.yeoli.devlog.domain.memo.repository
+
+import com.github.yeoli.devlog.domain.memo.domain.Memo
+import java.time.LocalDateTime
+
+data class MemoState(
+    var id: Long = 0L,
+    var createdAt: String,
+    var updatedAt: String,
+    var content: String = "",
+    var commitHash: String? = null,
+    var filePath: String? = null,
+    var selectedCodeSnippet: String? = null,
+    var selectionStart: Int? = null,
+    var selectionEnd: Int? = null,
+    var visibleStart: Int? = null,
+    var visibleEnd: Int? = null
+) {
+    fun toDomain(): Memo =
+        Memo(
+            id = this.id,
+            createdAt = this.createdAt.let { LocalDateTime.parse(it) },
+            updatedAt = this.updatedAt.let { LocalDateTime.parse(it) },
+            content = content,
+            commitHash = commitHash,
+            filePath = filePath,
+            selectedCodeSnippet = selectedCodeSnippet,
+            selectionStart = selectionStart,
+            selectionEnd = selectionEnd,
+            visibleStart = visibleStart,
+            visibleEnd = visibleEnd
+        )
+}
