5008a28db413fd7bca8da0deabd1d7510abbe9d3
yeo-li <parkseongyeol2110@gmail.com>
2025-11-24 01:40:54 +0900

    feat(NoteRepository): NoteRepository 작성

---
+++ src/main/kotlin/com/github/yeoli/devlog/domain/note/repository/NoteRepository.kt
@@ -0,0 +1,36 @@
+package com.github.yeoli.devlog.domain.note.repository
+
+import com.github.yeoli.devlog.domain.note.domain.Note
+import com.intellij.openapi.components.PersistentStateComponent
+import com.intellij.openapi.components.Service
+import com.intellij.openapi.components.State
+import com.intellij.openapi.components.Storage
+import java.time.LocalDateTime
+
+@State(
+    name = "DevLogMemoStorage",
+    storages = [Storage("devlog-memos.xml")]
+)
+@Service(Service.Level.PROJECT)
+class NoteRepository : PersistentStateComponent<NoteState> {
+
+    private var state: NoteState? = NoteState(
+        content = "",
+        updatedAt = LocalDateTime.now().toString()
+    )
+
+    override fun getState(): NoteState? = state
+
+    override fun loadState(state: NoteState) {
+        this.state = state
+    }
+
+    fun getNote(): Note {
+        if (state == null) {
+            state = Note(
+                content = ""
+            ).toState()
+        }
+        return state!!.toDomain()
+    }
+}
