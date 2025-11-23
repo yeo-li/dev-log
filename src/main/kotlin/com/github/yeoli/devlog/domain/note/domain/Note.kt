a07ba3d13d764f346b6160992a055ced990b32b1
yeo-li <parkseongyeol2110@gmail.com>
2025-11-24 01:21:55 +0900

    feat(Note): toState 메서드 구현

--- src/main/kotlin/com/github/yeoli/devlog/domain/note/domain/Note.kt
+++ src/main/kotlin/com/github/yeoli/devlog/domain/note/domain/Note.kt
@@ -1,5 +1,6 @@
 package com.github.yeoli.devlog.domain.note.domain
 
+import com.github.yeoli.devlog.domain.note.repository.NoteState
 import java.time.LocalDateTime
 
 class Note(
@@ -12,6 +13,13 @@ class Note(
     fun update(content: String): Note {
         return Note(
             content = content
+        )
+    }
+
+    fun toState(): NoteState {
+        return NoteState(
+            content = this.content,
+            updatedAt = this.updatedAt.toString()
         )
     }
 }
